8c8a796f8e8188abe7ff48a8e8583bb231e126ed
Akim Mukhtarov <akim.int80h@gmail.com>
2022-06-05 01:02:07 +0400

    first blueprint of base views

--- televiewshka/view.py
+++ televiewshka/view.py
@@ -1,1 +1,28 @@
+from abc import ABC, abstractmethod
+from .layouts import KeyboardLayout
+from .adaptors.base import Message
 
+
+class AbstractBaseView(ABC):
+    def __init__(self, user):
+        self.user = user
+
+
+class KeyboardView(AbstractBaseView):
+    @staticmethod
+    @abstractmethod
+    def render(*args, **kwargs) -> KeyboardLayout:
+        pass
+
+
+class TextView(AbstractBaseView):
+    @staticmethod
+    @abstractmethod
+    def render(*args, **kwargs) -> str:
+        pass
+
+
+class TextDialogView(TextView):
+    @abstractmethod
+    def on_message(self, message: Message):
+        pass
